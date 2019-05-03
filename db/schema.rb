f6ef114a528de75cbbf5106d5ade8b0387b791bd
Justin Kang <jmkang06@gmail.com>
2019-05-03 17:55:53 -0500

    [NXM-5] Add par and type to challenges (#10)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_075204) do
+ActiveRecord::Schema.define(version: 2019_05_03_224850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_04_13_075204) do
     t.float "rating"
     t.string "starter_code"
     t.integer "creator_id", null: false
+    t.integer "par"
+    t.string "type"
     t.index ["creator_id"], name: "index_challenges_on_creator_id"
   end
 
